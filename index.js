e581802786c1563e2890dcb40a607c139559d15c
Kellymugambi <kellymgambi@.com>
2022-11-26 21:15:59 +0300

    copleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(defaultActivity = "roller-skate") {
+    return `This Saturday, I want to ${defaultActivity}!`
+}
+
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (special = "||") {
+    return function (adjective = "a dedicated programmer"){
+        return `You are ${special}${adjective}${special}!`
+    }
+}
